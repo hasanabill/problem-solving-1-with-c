e12a521a4f83e093b94678af1ad8894eaf7df6a1
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2022-08-29 22:18:12 +0600

    problem 13 solved

--- problem-13/main.c
+++ problem-13/main.c
@@ -6,6 +6,14 @@ If a five-digit number is input through the keyboard, write a program to print a
 
 int main()
 {
+    int num, newNum;
+
+    printf("Enter a five digit number: ");
+    scanf("%d", &num);
+
+    newNum = num + 11111;
+
+    printf("The number after increasing each of its digit by 1 is %d", newNum);
 
     return 0;
 }
