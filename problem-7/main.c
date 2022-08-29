522dea46967c5092489eed4d5b25183d841fb23d
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2022-08-29 20:29:15 +0600

    7 organized

--- problem-7/main.c
+++ problem-7/main.c
@@ -9,12 +9,15 @@ int main()
     int num, a, b, c, d, e, sum;
     printf("Enter a five digit number : ");
     scanf("%d", &num);
-    e = num % 10;
-    d = (num / 10) % 10;
-    c = (num / 100) % 10;
+    a = num / 10000;
     b = (num / 1000) % 10;
-    a = (num / 10000);
+    c = (num / 100) % 10;
+    d = (num / 10) % 10;
+    e = num % 10;
+
     sum = a + b + c + d + e;
-    printf("%d is the sum of the digits of the number %d.", sum, num);
+
+    printf("The sum of the digits of the number %d is %d.", num, sum);
+
     return 0;
 }
